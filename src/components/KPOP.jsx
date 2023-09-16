0b82b4d75afa4e6ff15b09ea7384acd0938b898a
Francine Borromeo <francineysabelle2@gmail.com>
2023-09-16 18:04:14 +0800

    with shimmers?maybe

--- src/components/KPOP.jsx
+++ src/components/KPOP.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import SEVENTEEN from "../img/SEVENTEEN.jpeg";
 import RTL from "../img/readytolove.jpeg";
+import FML from "../img/fml.png";
 function KPOP(){
     return (
         <div className = "carousel-container row">
@@ -20,21 +21,15 @@ function KPOP(){
                         <h2>Ready To Love Era</h2>
                     </Carousel.Caption>
                     </Carousel.Item>
+                    <Carousel.Item>
+                    <img src = {FML} alt = "fml" />
+                    <Carousel.Caption>
+                        <h2>Super Era</h2>
+                    </Carousel.Caption>
+                    </Carousel.Item>
                 </Carousel>
             </div>
         
-
-        <div className = "d-flex align-items-center justify-content-center">
-            
-            <p>Seventeen is my all time favorite kpop group.</p>
-            <p>Some fun facts about the group: </p>
-            <ul>
-                <li>Seventeen has 3 subunits: Hiphop team, Performance team, Vocal team, and BSS.</li>
-                <li>There are 2 Chinese members and 2 American members.</li>
-                <li>They are a self-producing group. </li>
-            </ul>
-
-        </div>
        </div> 
     )
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from '../components/Home';
 import AboutMe from '../components/AboutMe';
+import KPOP from '../components/KPOP';
 
 function MainLayout(){
     return(
         <div>
             <Home />
             <AboutMe />
+            <KPOP />
         </div>
         
     );
